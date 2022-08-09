3b25d11fe18e25145af8acfe5ca3b30ab1ef2c43
Nicola Da Rold <nicola.darold@gmail.com>
2022-08-09 14:36:01 +0200

    empty generator LinearComponent

---
+++ src/app/generators/generators.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LinearComponent } from './linear/linear.component';
+
+
+
+@NgModule({
+  declarations: [
+    LinearComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class GeneratorsModule { }
---
+++ src/app/generators/linear/linear.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-linear',
+  templateUrl: './linear.component.html',
+  styleUrls: ['./linear.component.sass']
+})
+export class LinearComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
